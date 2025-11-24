851e5ae4cdef73e0de2c334d964db91163aaf8a2
Christina <christinamarieelaine@gmail.com>
2025-11-23 20:17:09 -0800

    exercise 9

--- js/app.js
+++ js/app.js
@@ -113,3 +113,22 @@ console.log(foods.indexOf("tofu"));
 const soyIdx = foods [3]
 
 console.log('Exercise 8 result:', soyIdx);
+
+
+/*
+Exercise 9: Joining elements
+
+1) Use the `join()` method to concatenate the strings in the `foods` array, 
+   separated by ' -> '. 
+
+2) Assign the result to a variable called `allFoods`. 
+
+Note: The final result should log as:
+'taco -> sushi -> cupcake -> tofu -> cheeseburger'
+
+Complete Exercise 9 in the space below:
+*/
+
+const allFoods = foods.join(" -> ");
+
+console.log('Exercise 9 result:', allFoods);
